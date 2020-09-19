f717bf606c02c3233d2f9155471e0b4d57c861f8
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-09-19 13:01:02 +0530

    [mster] initial skeleton with minimal hooks and screens 🧑🏻‍💻

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screen components
+import CardCategories from './screens/card-categories';
+import useCachedResources from './hooks/useCachedResources';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const isLoadingResources = useCachedResources();
+
+  if (isLoadingResources) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        
+      </SafeAreaProvider>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
